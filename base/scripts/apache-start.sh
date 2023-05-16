a7abdbc112096233514c04b235f91b7c1dcf6523
kha <kha@devpanel.com>
2023-05-16 18:10:21 +0700

    Add PM2 for nodejs

--- base/scripts/apache-start.sh
+++ base/scripts/apache-start.sh
@@ -3,19 +3,7 @@
 
 sudo cp /templates/apache2.conf /etc/apache2/apache2.conf
 sudo cp /templates/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-sudo cp /templates/php.ini ${PHP_EXT_DIR}/zz-www.ini
-
-# Substitute in php.ini values
-[ ! -z "${PHP_CLEAR_ENV}" ] && sed -i "s|{{PHP_CLEAR_ENV}}|${PHP_CLEAR_ENV}|" ${PHP_EXT_DIR}/zz-www.ini
-[ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s|{{PHP_MEMORY_LIMIT}}|${PHP_MEMORY_LIMIT}|" ${PHP_EXT_DIR}/zz-www.ini
-[ ! -z "${PHP_UPLOAD_MAX_FILESIZE}" ] && sed -i "s|{{PHP_UPLOAD_MAX_FILESIZE}}|${PHP_UPLOAD_MAX_FILESIZE}|" ${PHP_EXT_DIR}/zz-www.ini
-[ ! -z "${PHP_POST_MAX_SIZE}" ] && sed -i "s|{{PHP_POST_MAX_SIZE}}|${PHP_POST_MAX_SIZE}|" ${PHP_EXT_DIR}/zz-www.ini
-[ ! -z "${PHP_MAX_EXECUTION_TIME}" ] && sed -i "s|{{PHP_MAX_EXECUTION_TIME}}|${PHP_MAX_EXECUTION_TIME}|" ${PHP_EXT_DIR}/zz-www.ini
-[ ! -z "${PHP_MAX_INPUT_TIME}" ] && sed -i "s|{{PHP_MAX_INPUT_TIME}}|${PHP_MAX_INPUT_TIME}|" ${PHP_EXT_DIR}/zz-www.ini
-[ ! -z "${PHP_MAX_INPUT_VARS}" ] && sed -i "s|{{PHP_MAX_INPUT_VARS}}|${PHP_MAX_INPUT_VARS}|" ${PHP_EXT_DIR}/zz-www.ini
-
-# Add custom php.ini if it exists
-[ -f "$PHP_CUSTOM_INI" ] && sudo cp $PHP_CUSTOM_INI ${PHP_EXT_DIR}/zzz-www-custom.ini
+PM2_START_SCRIPT=$APP_ROOT/ecosystem.config.js
 
 # Custom Environment variables in /etc/apache2/sites-enabled/000-default.conf
 [ ! -z "$WEB_ROOT" ] &&  sudo sed -i "s|{{WEB_ROOT}}|${WEB_ROOT}|" /etc/apache2/sites-enabled/000-default.conf
@@ -33,7 +21,10 @@ sudo  apachectl -D FOREGROUND &
 sudo -u www -E -- code-server --auth none --port $CODES_PORT --host 0.0.0.0 $CODES_WORKING_DIR
 # and leave it there
 fg %1
+
+[ -f "$PM2_START_SCRIPT" ] && pm2 start $PM2_START_SCRIPT
 else
 # Start the primary process and put it in the background
 sudo  apachectl -D FOREGROUND
+[ -f "$PM2_START_SCRIPT" ] && pm2 start $PM2_START_SCRIPT
 fi
